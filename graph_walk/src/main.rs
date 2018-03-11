f96bb4cf4d51b673cbb1d918bc70b9b419960450
John Geer <john@jgeer.com>
2018-03-11 15:29:14 -0400

    Get it to parse tx_in

--- graph_walk/src/main.rs
+++ graph_walk/src/main.rs
@@ -11,21 +11,26 @@ extern crate rocksdb;
 use std::error::Error;
 use std::io;
 use std::process;
-use std::str;
+use std::str;       // strings
 
-use rand::Rng;
-use rocksdb::DB;
+use rand::Rng;      // random number generation
+use rocksdb::DB;    // working with the rocksdb
 
 const TX_OUT_DB: &'static str = "../graph_data/tx_out_rdb";
 const TX_IN_DB: &'static str = "../graph_data/tx_in_rdb";
 
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 struct TxIn {
     txid: String,
     hashprevout: String,
     indexprevout: String,
     scriptsig: String,
     sequence: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+struct TxInSm {
+    txid: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -45,58 +50,98 @@ struct TxOutSm {
     address: String,
 }
 
-fn get_present(db: &DB, key: String) -> Vec<TxOutSm> {
+fn get_present_out(db: &DB, key: String) -> Vec<TxOutSm> {
     match db.get(key.as_bytes()) {
         Ok(Some(value)) => serde_json::from_slice(&value[..]).unwrap(),
         Ok(None) => Vec::new(), // empty vector
         Err(e) => panic!("{}", e)
     }
 }
-fn put_record(db: &DB, key: String, value: Vec<TxOutSm>) {
+fn get_present_in(db: &DB, key: String) -> Vec<TxInSm> {
+    match db.get(key.as_bytes()) {
+        Ok(Some(value)) => serde_json::from_slice(&value[..]).unwrap(),
+        Ok(None) => Vec::new(), // empty vector
+        Err(e) => panic!("{}", e)
+    }
+}
+
+fn put_record<T>(db: &DB, key: String, value: Vec<T>)
+    where T: serde::Serialize {
     db.put(
         key.as_bytes(),
         serde_json::to_string(&value).unwrap().as_bytes());
 }
-fn extend_v<T>(mut present_v: Vec<T>, new_entry: T) -> Vec<T>
+fn no_prev(key: &str) -> bool {
+    // Predicate of whether there is no previous for this transaction
+    return key == "00000000000000000000000000000000000000000000000000000000000000004294967295";
+}
+fn extend_v<T>(mut present_v: Vec<T>, new_entry: T, key: &str) -> Vec<T>
     where T: Ord + PartialEq {
-    present_v.push(new_entry);  // add new value
-    present_v.sort();           // sort, to enable removing duplicates
-    present_v.dedup();          // remove duplicates
-    return present_v;
+    if no_prev(key) {
+        // In this case they are all unique (no duplicates), and it is a really big list
+        return present_v;
+    } else {
+        present_v.push(new_entry);  // add new value
+        present_v.sort();           // sort, to enable removing duplicates
+        present_v.dedup();          // remove duplicates
+        return present_v;
+    }
 }
 
-fn save_tx_out() -> Result<(), Box<Error>> {
+fn save_tx(direction: &str) -> Result<(), Box<Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_reader(io::stdin());
     // Connect to the rocksdb database
-    let db = DB::open_default(TX_OUT_DB).unwrap();
-    for result in rdr.deserialize() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here..
-        let record: TxOut = result?;    // parse CSV
-        let value = TxOutSm {           // Convert to smaller type
-            indexout: record.indexout,
-            value: record.value,
-            address: record.address
-        };
-        // Check what we already have stored
-        let tx_out_v = get_present(&db, record.txid.clone());
-        put_record(&db, record.txid.clone(), extend_v(tx_out_v, value));
-
+    let db_location: &str = match direction {
+        "tx_in" => TX_IN_DB,
+        _ => TX_OUT_DB,
+    };
+    let db = DB::open_default(db_location).unwrap();
+    if direction == "tx_in" {
+        for result in rdr.deserialize() {
+            // The iterator yields Result<StringRecord, Error>, so we check the
+            // error here..
+            let record: TxIn = result?;         // parse CSV
+            let new_entry = TxInSm {            // Convert to smaller type
+                txid: record.txid,
+            };
+            // Check what we already have stored
+            let key: String = format!("{}{}", &record.hashprevout, &record.indexprevout);
+            let tx_v: Vec<TxInSm> = get_present_in(&db, key.clone());
+            put_record(&db, key.clone(), extend_v(tx_v, new_entry, &key));
+        }
+    } else {
+        for result in rdr.deserialize() {
+            let record: TxOut = result?;        // parse CSV
+            let new_entry = TxOutSm {           // Convert to smaller type
+                indexout: record.indexout,
+                value: record.value,
+                address: record.address
+            };
+            // Check what we already have stored
+            let key: String = record.txid.clone();
+            let tx_v: Vec<TxOutSm> = get_present_out(&db, key.clone());
+            put_record(&db, key.clone(), extend_v(tx_v, new_entry, &key));
+        }
+    }
         // let rand_n = rand::thread_rng().gen_range(1, 1001);
         // if rand_n > 990 { // so that printing doesn't become the bottleneck
         //     let check = get_present(&db, record.txid.clone());
         //     println!("{}", serde_json::to_string(&check).unwrap());
         // }
-    }
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = save_tx_out() {
-        println!("error running save_tx_out: {}", err);
+    // if let Err(err) = save_tx_out() {
+    //     println!("error running save_tx_out: {}", err);
+    //     process::exit(1);
+    // }
+    if let Err(err) = save_tx("tx_in") {
+        println!("error running save_tx: {}", err);
         process::exit(1);
     }
 }
 
 // cargo run < ../csv-data/tx_out_sample.csv
+// cargo run < ../csv-data/tx_in_sample.csv
